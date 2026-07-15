fdc5683b2f83fc31bc86eed42185ee4ccd6d20b5
atammorey-lgtm <atam.morey@gmail.com>
2026-07-15 11:57:23 -0600

    Add files via upload

---
+++ Exploratory Data Analysis.sql
@@ -0,0 +1,187 @@
+---- Exploratory project
+
+SELECT *
+FROM layoffs_staging2;
+
+SELECT MAX(total_laid_off), MAX(percentage_laid_off)
+FROM layoffs_staging2;
+
+SELECT * 
+FROM layoffs_staging2
+WHERE percentage_laid_off = 1
+;
+
+SELECT *
+FROM layoffs_staging2
+WHERE percentage_laid_off = 1
+ORDER BY total_laid_off DESC
+;
+
+SELECT *
+FROM layoffs_staging2
+WHERE percentage_laid_off = 1
+ORDER BY funds_raised_millions DESC
+;
+
+SELECT company, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC
+;
+
+SELECT MIN(`date`), MAX(`date`)
+FROM layoffs_staging2
+;
+
+SELECT industry, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY industry
+ORDER BY 2 DESC
+;
+
+SELECT country, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY country
+ORDER BY 2 DESC
+;
+
+SELECT YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY YEAR(`date`)
+ORDER BY 1 DESC
+;
+
+SELECT stage, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY stage
+ORDER BY 2 DESC
+;
+
+SELECT SUBSTRING(`date`,6,2) AS `Month`, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY `Month`
+;
+
+SELECT SUBSTRING(`date`,1,7) AS `Month`, SUM(total_laid_off)
+FROM layoffs_staging2
+WHERE SUBSTRING(`date`,1,7)
+GROUP BY `Month`
+ORDER BY 1 ASC
+;
+
+WITH Rolling_total AS
+(
+SELECT SUBSTRING(`date`,1,7) AS `Month`, SUM(total_laid_off) AS total_off
+FROM layoffs_staging2
+WHERE SUBSTRING(`date`,1,7) IS NOT NULL
+GROUP BY `Month`
+ORDER BY 1 ASC
+)
+
+SELECT `Month`, SUM(total_off) OVER(ORDER BY`Month`) AS rolling_total
+FROM Rolling_Total
+;
+
+WITH Rolling_total AS
+(
+SELECT SUBSTRING(`date`,1,7) AS `Month`, SUM(total_laid_off) AS total_off, country
+FROM layoffs_staging2
+WHERE SUBSTRING(`date`,1,7) IS NOT NULL
+GROUP BY `Month`, country
+ORDER BY 1 ASC
+)
+SELECT country, `Month`, total_off
+,SUM(total_off) OVER(partition by country ORDER BY`Month`) AS rolling_total
+FROM Rolling_Total
+;
+
+Select *
+FROM layoffs_staging2
+;
+
+SELECT company, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC
+;
+
+SELECT company,
+SUBSTRING(`date`,1,4) AS YEAR,
+SUM(total_laid_off) AS total_off
+FROM layoffs_staging2
+GROUP BY company, YEAR
+ORDER BY YEAR
+;
+
+WITH Company_year (company, years, total_laid_off) AS
+(
+SELECT company,
+YEAR (`date`),
+SUM(total_laid_off) AS total_off
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+)
+SELECT *, DENSE_RANK() OVER(PARTITION BY years ORDER BY total_laid_off DESC)
+FROM Company_year
+;
+
+WITH Company_year (company, years, total_laid_off) AS
+(
+SELECT company,
+YEAR (`date`),
+SUM(total_laid_off) AS total_off
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+)
+SELECT *, DENSE_RANK() OVER(PARTITION BY years ORDER BY total_laid_off DESC)
+FROM Company_year
+WHERE years IS NOT NULL
+;
+
+WITH Company_year (company, years, total_laid_off) AS
+(
+SELECT company,
+YEAR (`date`),
+SUM(total_laid_off) AS total_off
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+)
+SELECT *,
+DENSE_RANK() OVER(PARTITION BY years ORDER BY total_laid_off DESC) AS Ranking
+FROM Company_year
+WHERE years IS NOT NULL
+ORDER BY Ranking ASC
+;
+
+WITH Company_year (company, years, total_laid_off) AS
+(
+SELECT company,
+YEAR (`date`),
+SUM(total_laid_off) AS total_off
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+)
+SELECT *,
+DENSE_RANK() OVER(PARTITION BY years ORDER BY total_laid_off DESC) AS Ranking
+FROM Company_year
+WHERE years IS NOT NULL AND Ranking <= 5
+ORDER BY Ranking ASC
+;
+
+WITH Company_year (company, years, total_laid_off) AS
+(
+SELECT company,
+YEAR (`date`),
+SUM(total_laid_off) AS total_off
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+), Company_year_rank AS
+(SELECT *,
+DENSE_RANK() OVER(PARTITION BY years ORDER BY total_laid_off DESC) AS Ranking
+FROM Company_year
+WHERE years IS NOT NULL
+)
+SELECT *
+FROM Company_year_rank
+WHERE Ranking <= 5
+;
